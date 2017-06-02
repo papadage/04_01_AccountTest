1b48a452ff543588f36d09fbce2072c0d0973e4c
papadage <gpapadakis@aegis-net.com>
2017-06-01 22:10:10 -0400

    First version of AccountTest.

---
+++ ConsoleApp1/ConsoleApp1/Account.cs
@@ -0,0 +1,12 @@
+﻿// Figure 4.2: Account.cs
+// A simple Account class that contains a private instance
+//    variable name and public methods to Set and Get the nbame's value.
+
+class Account // Pascal class notation of the name
+{
+   private string name; // Instance varibale name belonging to class Account
+   // private - Only object will be able to read or manipulate the variable through its methods
+   // Keep instance variable declarations in the beginning out of convention
+   // Each instance variable will have a default value based on its type (null for strings), or
+   //    can set defaults.
+}
---
+++ ConsoleApp1/ConsoleApp1/AccountTest.cs
@@ -0,0 +1,25 @@
+﻿// Figure 4.1: AccountTest.cs
+// Creating adnd Manipulating an Account Object
+
+using System;
+
+class AccountTest // Create a class
+{ //Open Accounttest class
+   static void Main() // Create Main method
+   { // Open Main
+      // Create an Account object and assign it to myAccount
+      Account myAccount = new Account();
+
+      // Display myAccount's initial name (there isn't one)
+      Console.WriteLine($"Initial name is: { myAccount.GetName()}");
+
+      // Prompt for and read the name, then put the name in the object
+      Console.Write("Enter the name:"); // Prompt
+      string theName = Console.ReadLine(); // Read the name
+      myAccount.SetName(theName); // Put theName into the myAccount object
+
+      // Display the name stored in the myAccount object
+      Console.WriteLine($"myAccount's name is: [myAccount.GetName()}");
+
+   } // Close Main
+} // Close AccountTest Class
