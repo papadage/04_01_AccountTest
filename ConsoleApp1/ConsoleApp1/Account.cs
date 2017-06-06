08ab10527b28463cb54f623f2658d1e204f91669
papadage <gpapadakis@aegis-net.com>
2017-06-06 15:02:47 -0400

    Additional time from study session. Added SetName and GetName methods to Account class

--- ConsoleApp1/ConsoleApp1/Account.cs
+++ ConsoleApp1/ConsoleApp1/Account.cs
@@ -9,4 +9,19 @@ class Account // Pascal class notation of the name
    // Keep instance variable declarations in the beginning out of convention
    // Each instance variable will have a default value based on its type (null for strings), or
    //    can set defaults.
+
+   // Method that sets the account name in the object
+   public void SetName(string accountName) // usable by method client, with no return, and a parameter
+   {
+      name = accountName; // store the account name in the instance variable of the class
+   }
+
+   // Method retrieves acccount name from the object
+   public string GetName()
+   // public, so usable outside the class
+   {
+      return name; // Returns name's vlaue to method's caller
+      // Not void since it will return a value
+   }
+
 }
