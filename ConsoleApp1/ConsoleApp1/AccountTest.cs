10f285cb488d9a2f4e3cf5fae93e3d2897ba4d5c
papadage <gpapadakis@aegis-net.com>
2017-06-26 20:35:36 -0400

    Practice typing commands after lack of studying for a while.

--- ConsoleApp1/ConsoleApp1/AccountTest.cs
+++ ConsoleApp1/ConsoleApp1/AccountTest.cs
@@ -1,6 +1,7 @@
 ﻿// Figure 4.1: AccountTest.cs
 // Creating adnd Manipulating an Account Object
 
+
 using System;
 
 class AccountTest // Create a class
@@ -23,3 +24,20 @@ class AccountTest // Create a class
 
    } // Close Main
 } // Close AccountTest Class
+
+//using System;
+
+//class AccountTest // Create class
+//{
+//   static void Main() // Main method
+//   {
+//      Account myAccount = new Account(); // Instantiate new object from Account class and reference it with myAccount variable of Account type
+//      Console.WriteLine($"Initial name is: {myAccount.GetName()}"); // Use GetName method of Account class to show empty name
+//      Console.Write("Enter the name: "); // Prompt for input of name
+//      string theName = Console.ReadLine(); // Grab input as a string variable
+//      myAccount.SetName(theName); // Use Account method SetName to put variable value into name of object
+//      Console.WriteLine($"myAccount's name is {myAccount.GetName()}"); // Use GetName method of class Account to show the new name
+//   }
+//}
+
+
